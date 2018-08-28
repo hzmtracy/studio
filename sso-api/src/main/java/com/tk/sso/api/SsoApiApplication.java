89e1cc09ec87fcaf391c18ca7bbb8152c89dbe3d
hzmeric <huangzm@imaidao.cn>
2018-08-28 23:22:13 +0800

    init

---
+++ sso-api/src/main/java/com/tk/sso/api/SsoApiApplication.java
@@ -0,0 +1,12 @@
+package com.tk.sso.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApiApplication.class, args);
+    }
+}
