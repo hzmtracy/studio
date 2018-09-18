1a62d387be92886ca0869330f49b23d0347d1520
hzmeric <huangzm@imaidao.cn>
2018-09-18 23:26:26 +0800

    init

--- sso-api/src/test/java/com/tk/sso/api/SsoApiApplicationTests.java
+++ sso-api/src/test/java/com/tk/sso/api/SsoApiApplicationTests.java
@@ -1,7 +1,12 @@
 package com.tk.sso.api;
 
+
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tk.sso.api.test.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SsoApiApplicationTests {
 
+    @Autowired
+    private Person person;
+
     @Test
-    public void contextLoads() {
+    public void propsTest() throws JsonProcessingException {
+        System.out.println("simpleProp: " + JSON.toJSONString(person));
+
+//        ObjectMapper objectMapper =;
+//        System.out.println("arrayProps: " + objectMapper.writeValueAsString(myProps.getArrayProps()));
+//        System.out.println("listProp1: " + objectMapper.writeValueAsString(myProps.getListProp1()));
+//        System.out.println("listProp2: " + objectMapper.writeValueAsString(myProps.getListProp2()));
+//        System.out.println("mapProps: " + objectMapper.writeValueAsString(myProps.getMapProps()));
     }
+
 
 }
